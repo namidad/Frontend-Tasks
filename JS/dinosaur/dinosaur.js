24a967052ce948185865ba23f9b61dc0eb06aa96
Oliwier Salamon <225990@student.pwr.edu.pl>
2019-02-19 15:26:26 +0100

    Add files via upload
    added few obstacles

--- JS/dinosaur/dinosaur.js
+++ JS/dinosaur/dinosaur.js
@@ -9,6 +9,8 @@ class Dinosaur {
       if(this.yspeed===0){
         this.xspeed=x;
         this.yspeed=y;
+      } else {
+        this.xspeed=x;
       }
     }
 
@@ -40,7 +42,7 @@ class Dinosaur {
         this.yspeed=-4;
       } else if (this.yspeed===-4){
         for(let i=0;i<this.dinosaur.length;i++){
-          this.dinosaur[i].y=this.dinosaur[i].y+10;
+          this.dinosaur[i].y=this.dinosaur[i].y-10;
         }
         this.yspeed=-5;
       } else if (this.yspeed===-5){
@@ -49,6 +51,16 @@ class Dinosaur {
         }
         this.yspeed=-6;
       } else if (this.yspeed===-6){
+        for(let i=0;i<this.dinosaur.length;i++){
+          this.dinosaur[i].y=this.dinosaur[i].y+10;
+        }
+        this.yspeed=-7;
+      } else if (this.yspeed===-7){
+        for(let i=0;i<this.dinosaur.length;i++){
+          this.dinosaur[i].y=this.dinosaur[i].y+10;
+        }
+        this.yspeed=-8;
+      } else if (this.yspeed===-8){
         for(let i=0;i<this.dinosaur.length;i++){
           this.dinosaur[i].y=this.dinosaur[i].y+10;
         }
--- JS/dinosaur/obstacle.js
+++ JS/dinosaur/obstacle.js
@@ -1,19 +1,58 @@
 class Obstacle {
   constructor(){
+    this.timeLeft=0;
     this.obstacle = [{x:590, y:590},[{x:690,y:590},{x:690,y:580}]];
+  }
+
+  newObstacle(){
+    if(this.timeLeft>20){
+      let x = Math.floor(Math.random() * 10);
+      if(x<=6){
+        x = Math.floor(Math.random() * 10);
+        if(x===0){
+          this.obstacle.push({x:590, y:590});
+        } else if(x===1){
+          this.obstacle.push([{x:590, y:590},{x:600, y:590}]);
+        } else if(x===2){
+          this.obstacle.push([{x:590, y:590},{x:590,y:580}]);
+        } else if(x===3){
+          this.obstacle.push([{x:590, y:590},{x:590,y:580},{x:600,y:590}]);
+        } else if(x===4){
+          this.obstacle.push([{x:590, y:590},{x:590,y:580},{x:600,y:590},{x:600,y:580}]);
+        } else if(x===5){
+          this.obstacle.push([{x:590, y:590},{x:590,y:580},{x:590,y:570}]);
+        } else if(x===6){
+          this.obstacle.push([{x:590, y:590},{x:590,y:580},{x:590,y:570},{x:580, y:590}]);
+        } else if(x===7){
+          this.obstacle.push([{x:590, y:590},{x:590,y:580},{x:590,y:570},{x:580, y:590},{x:580,y:580}]);
+        } else if(x===8){
+          this.obstacle.push([{x:590, y:590},{x:580,y:590},{x:570,y:590}]);
+        } else {
+          this.obstacle.push([{x:590, y:590},{x:580,y:590},{x:590,y:500},{x:580, y:500}]);
+        }
+      }
+      this.timeLeft=0;
+    }
+
   }
 
   update(){
     for(let i=0;i<this.obstacle.length;i++){
       if(!Array.isArray(this.obstacle[i])){
         this.obstacle[i].x=this.obstacle[i].x-10;
+        if(this.obstacle[i].x<0){
+          this.obstacle.splice(0,1);
+          i=0;
+        }
+
       } else {
         for(let j=0;j<this.obstacle[i].length;j++){
           this.obstacle[i][j].x=this.obstacle[i][j].x-10;
         }
       }
-
     }
+    this.newObstacle();
+    this.timeLeft++;
   }
 
   show(){
