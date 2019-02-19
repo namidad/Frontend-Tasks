3da30e56d191001700923f244390b2931d43f042
Oliwier Salamon <225990@student.pwr.edu.pl>
2019-02-19 12:29:19 +0100

    Add files via upload

--- JS/dinosaur/dinosaur.js
+++ JS/dinosaur/dinosaur.js
@@ -1,8 +1,56 @@
-class Snake {
+class Dinosaur {
     constructor() {
       this.x = 0;
       this.y = 0;
       this.xspeed=1;
       this.yspeed=0;
+      this.dinosaur = [createVector(0,590), createVector(20,590), createVector(10,580)];
     }
+
+    dir(x,y){
+      this.xspeed=x;
+      this.yspeed=y;
+    }
+
+    update(){
+      if(this.xspeed===1){
+        for(let i=0;i<this.dinosaur.length;i++){
+          this.dinosaur[i].x=this.dinosaur[i].x+10;
+        }
+        this.xspeed=0;
+      } else if(this.xspeed===-1){
+        for(let i=0;i<this.dinosaur.length;i++){
+          this.dinosaur[i].x=this.dinosaur[i].x-10;
+        }
+        this.xspeed=0;
+      } else if(this.yspeed===-1){
+        for(let i=0;i<this.dinosaur.length;i++){
+          this.dinosaur[i].y=this.dinosaur[i].y-10;
+        }
+        this.show();
+        for(let i=0;i<this.dinosaur.length;i++){
+          this.dinosaur[i].y=this.dinosaur[i].y-10;
+        }
+        this.show();
+        for(let i=0;i<this.dinosaur.length;i++){
+          this.dinosaur[i].y=this.dinosaur[i].y+10;
+        }
+        this.show();
+        for(let i=0;i<this.dinosaur.length;i++){
+          this.dinosaur[i].y=this.dinosaur[i].y+10;
+        }
+        this.show();
+        this.yspeed=0;
+      }
+
+    }
+
+    show(){
+        fill(255);
+        for(let i=0;i<this.dinosaur.length;i++){
+          rect(this.dinosaur[i].x,this.dinosaur[i].y,scl,scl);
+        }
+
+      }
+
   }
--- JS/dinosaur/game.js
+++ JS/dinosaur/game.js
@@ -1,18 +1,31 @@
 let dinosaur;
-let scl = 20;
+let scl = 10;
 
 
 function setup() {
   createCanvas(600, 600);
   frameRate(10);
-  pickLocation();
+  dinosaur=new Dinosaur();
 }
 
 
 
 function draw() {
   background(51);
-
+  dinosaur.update();
+  dinosaur.show();
   fill(255,0,100);
-  rect(food.x, food.y, scl, scl);
+}
+
+
+function keyPressed() {
+  if(keyCode === UP_ARROW){
+    dinosaur.dir(0,-1)
+  } else if (keyCode === DOWN_ARROW) {
+    dinosaur.dir(0,1);
+  } else if (keyCode === RIGHT_ARROW) {
+    dinosaur.dir(1,0);
+  } else if (keyCode === LEFT_ARROW) {
+    dinosaur.dir(-1,0);
+  }
 }
